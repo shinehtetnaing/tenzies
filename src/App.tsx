459718aa2cc2b607c24d58a4f863fe97680235f5
shinehtetnaing <shinehtetnaing1998@gmail.com>
2025-11-19 15:37:29 +0700

    generate dice with random number & roll dice function

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,23 @@
+import { useState } from "react";
 import Die from "./components/Die";
 
 function App() {
+  const [dice, setDice] = useState(generateAllNewDice());
+
+  function generateAllNewDice() {
+    return new Array(10).fill(0).map(() => Math.ceil(Math.random() * 6));
+    // const newDice = [];
+    // for (let i = 0; i < 10; i++) {
+    //   const rand = Math.ceil(Math.random() * 6);
+    //   newDice.push(rand);
+    // }
+    // return newDice;
+  }
+
+  const rollDice = () => {
+    setDice(generateAllNewDice());
+  };
+
   return (
     <main className="bg-main-bg flex h-[calc(100vh-40px)] flex-col items-center justify-center rounded p-16">
       <h1 className="mb-3 text-4xl font-bold">Tenzies</h1>
@@ -9,18 +26,14 @@ function App() {
         current value between rolls.
       </p>
       <div className="my-10 grid grid-cols-5 gap-4 sm:gap-8 md:gap-12">
-        <Die value={1} />
-        <Die value={2} />
-        <Die value={3} />
-        <Die value={4} />
-        <Die value={5} />
-        <Die value={6} />
-        <Die value={1} />
-        <Die value={2} />
-        <Die value={3} />
-        <Die value={4} />
+        {dice.map((value, index) => (
+          <Die key={index} value={value} />
+        ))}
       </div>
-      <button className="bg-btn-bg cursor-pointer rounded-lg px-12 py-3 text-lg text-white">
+      <button
+        onClick={rollDice}
+        className="bg-btn-bg cursor-pointer rounded-lg px-12 py-3 text-lg text-white"
+      >
         Roll
       </button>
     </main>
